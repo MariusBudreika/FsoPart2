5543cd74c86db866af0fb36ccb7781e561daa924
MariusB <marius.budreika09@gmail.com>
2026-03-10 12:28:15 +0200

    exercise 2.2 (sum)

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,13 +1,16 @@
 function Course({ course }) {
+  let total = 0;
+
   return (
     <div>
       <h1>{course.name}</h1>
       <div>
         {course.parts.map((part) => (
           <p key={part.id}>
-            {part.name} {part.exercises}
+            {part.name} {part.exercises} {(total += part.exercises)}
           </p>
         ))}
+        <p style={{ fontWeight: "bold" }}>Total of {total} exercises</p>
       </div>
     </div>
   );
